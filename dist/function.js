06d5f141c027f67e248c27d24fbe0852c7ab6499
Het <hra.opash@gmail.com>
2026-01-08 10:47:34 +0530

    funtion

---
+++ dist/function.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function makeChai(type, cups) {
+    console.log(`Making ${cups} cups of ${type}`);
+}
+makeChai("Masala", 2);
+//# sourceMappingURL=function.js.map
---
+++ dist/object.js
@@ -0,0 +1,13 @@
+"use strict";
+// const chai = {
+//   name: "Masala Chai",
+//   price: 20,
+//   isHot: true,
+// };
+Object.defineProperty(exports, "__esModule", { value: true });
+let chaiInfo = {
+    name: "chai",
+    price: 25,
+    isHot: true,
+};
+//# sourceMappingURL=object.js.map
